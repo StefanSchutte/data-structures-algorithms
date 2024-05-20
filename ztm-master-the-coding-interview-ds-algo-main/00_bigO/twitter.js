ae8b1c13178c325cb0d8b704e6c902cd6deaf4fc
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-05-20 21:57:50 +0200

    slv-cdng-prblm

---
+++ ztm-master-the-coding-interview-ds-algo-main/00_bigO/twitter.js
@@ -0,0 +1,15 @@
+//find 1st, find Nth
+const array = [{
+    tweet: 'hi',
+    date: 2012
+}, {
+    tweet: 'my',
+    date: 2014,
+}, {
+    tweet: 'teddy',
+    date: 2018,
+}];//)(n^2)
+
+array[0]; //O(1)
+array[array.length - 1] //O(1)
+
--- ztm-master-the-coding-interview-ds-algo-main/GoogleInterview.js
+++ ztm-master-the-coding-interview-ds-algo-main/GoogleInterview.js
@@ -58,5 +58,31 @@ function containsCommonItem(arr1, arr2) {
   containsCommonItem(array1, array2)
   containsCommonItem2(array1, array2)
   containsCommonItem3(array1, array2)
-  
-  
+
+// Naive
+function hasPairWithSum(arr, sum){
+  var len = arr.length;
+  for(var i = 0; i<len-1; i++) {
+    for (var j = i+1; j<len; j++) {
+      if (arr[i] + arr[j] === sum)
+        return true;
+    }
+  }
+
+  return false;
+}
+
+// Better
+function hasPairWithSum2(arr, sum) {
+  const mySet = new Set();
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (mySet.has(arr[i])) {
+      return true;
+    }
+    mySet.add(sum - arr[i]);
+  }
+  return false;
+}
+
+console.log(hasPairWithSum2([6,4,3,2,1,7], 9))
